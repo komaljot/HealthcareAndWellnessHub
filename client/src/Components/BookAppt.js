0c2a8fcdc811f18f0fd7cb4bac261ff55c18ab37
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-03-01 11:57:12 +0530

    basic login signup created

---
+++ client/src/Components/BookAppt.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function BookAppt() {
+  return (
+    <div>BookAppt</div>
+  )
+}
--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-export default function Login() {
-  return (
-    <>
-        <form>
-            
-        </form>
-    </>
-  )
+
+const Login=()=>{
+   const [initial, setInitital] = useState({
+    Username:'',
+    Password:''
+   });
+const changeHandler=e=>{
+    setInitital({...initial,[e.target.name]:e.target.value})
+    console.log(initial)
 }
+
+    return(
+        <div className="text-center my-5">
+            <h2 className="my-5">Login</h2>
+            <br />
+            <form action="">
+            <input type="text" placeholder="Username" onChange={changeHandler} name='Username'/>
+            <br /><br />
+            <input type="password" placeholder="Password" onChange={changeHandler} name='Password'/>
+            <br /><br />
+            </form>
+            <button>Login</button>
+            <br /><br />
+            <Link to="/">Sign Up Page </Link>
+        </div>
+    )
+}
+export default Login
