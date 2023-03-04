9440e4ba3bad231376d0b5ecb5ed79b35ab83e49
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-03-04 13:32:02 +0530

    Home page Header

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,17 @@ import { Route, Routes } from 'react-router-dom'
 import BookAppt from './Components/BookAppt'
 import Login from './Components/Login'
 import Signup from './Components/Signup'
-
+import Home from './Components/Home'
 
 
 export default function App() {
   return (
     <div>
       <Routes>
+        <Route path='/' element={<Home />}></Route>
         <Route path='/login' element={<Login />}></Route>
         <Route path='/signup' element={<Signup />}></Route>
+        <Route path='/appointment' element={<BookAppt />}></Route>
       </Routes>
     </div>
   )
--- client/src/Components/BookAppt.js
+++ client/src/Components/BookAppt.js
@@ -4,67 +4,178 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default function BookAppt() {
   return (
     <>
-      <form>
-        <div class="form-group row">
-          <label for="name" class="col-sm-2 col-form-label">Name</label>
-          <div class="col-sm-10">
-            <input type="email" class="form-control" id="name" placeholder="Enter Your Name" />
+      <div className='p-5'>
+        <h1 className='p-5 text-center'>Book Your Appointment</h1>
+        <form className='ms-5'>
+          <h3>Fill Patient Details : </h3>
+          <fieldset>
+          <div className="form-group row m-2">
+            <label for="name" className="col-sm-2 col-form-label">Name <span className='text-danger'>*</span></label>
+            <div className="col-sm-6">
+              <input type="text" className="form-control" id="name" placeholder="Enter Your Name" />
+            </div>
           </div>
-        </div>
-        <div class="form-group row">
-          <label for="email" class="col-sm-2 col-form-label">Email</label>
-          <div class="col-sm-10">
-            <input type="email" class="form-control" id="email" placeholder="Email" />
+          <div className="form-group row m-2">
+            <label for="age" className="col-sm-2 col-form-label">Age <span className='text-danger'>*</span></label>
+            <div className="col-sm-6">
+              <input type="number" className="form-control" id="age" placeholder="Enter Your Age" />
+            </div>
           </div>
-        </div>
-        <div class="form-group row">
-          <label for="phone" class="col-sm-2 col-form-label">Contact number</label>
-          <div class="col-sm-10">
-            <input type="email" class="form-control" id="email" placeholder="Contact Number" />
+          <div className="form-group row m-2">
+            <label className="col-sm-2 col-form-label">Gender <span className='text-danger'>*</span></label>
+            <div className="col-sm-6">
+            <div className="form-check">
+                <input className="form-check-input" type="radio" name="gender" id="male" value="male" checked />
+                <label className="form-check-label" for="male">
+                  Male
+                </label>
+              </div>
+              <div className="form-check">
+                <input className="form-check-input" type="radio" name="gender" id="female" value="female"  />
+                <label className="form-check-label" for="female">
+                  Female
+                </label>
+              </div>
+              <div className="form-check">
+                <input className="form-check-input" type="radio" name="gender" id="notsaid" value="notsaid"  />
+                <label className="form-check-label" for="notsaid">
+                  Prefer Not to say
+                </label>
+              </div>
+            </div>
           </div>
-        </div>
-  <fieldset class="form-group">
-    <div class="row">
-      <legend class="col-form-label col-sm-2 pt-0">Radios</legend>
-      <div class="col-sm-10">
-        <div class="form-check">
-          <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked />
-          <label class="form-check-label" for="gridRadios1">
-            First radio
-          </label>
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2" />
-          <label class="form-check-label" for="gridRadios2">
-            Second radio
-          </label>
-        </div>
-        <div class="form-check disabled">
-          <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled />
-          <label class="form-check-label" for="gridRadios3">
-            Third disabled radio
-          </label>
+          <div className="form-group row m-2">
+            <label for="address" className="col-sm-2 col-form-label">Address <span className='text-danger'>*</span></label>
+            <div className="col-sm-6">
+              <input type="text" className="form-control" id="address" placeholder="Address" />
+            </div>
+          </div>
+          <div className="form-group row m-2">
+            <label for="email" className="col-sm-2 col-form-label">Email <span className='text-danger'>*</span></label>
+            <div className="col-sm-6">
+              <input type="email" className="form-control" id="email" placeholder="Email" />
+            </div>
+          </div>
+          <div className="form-group row m-2">
+            <label for="phone" className="col-sm-2 col-form-label">Contact number <span className='text-danger'>*</span></label>
+            <div className="col-sm-6">
+              <input type="tel" className="form-control" id="email" placeholder="Contact Number" />
+            </div>
+          </div>
+          <div className="form-group row m-2 p-5">
+              <label><h3 className='text-center'>Schedule Available for this week Appointment</h3></label>
+              <table className='p-5'>
+                <tr className='p-5'>
+                  <th>Monday</th>
+                  <th>Tuesday</th>
+                  <th>Wednesday</th>
+                  <th>Thursday</th>
+                  <th>Friday</th>
+                  <th>Saturday</th>
+                </tr>
+                <tr className='p-5'>
+                  <td>Morning : </td>
+                  <td>Morning : </td>
+                  <td>Morning : </td>
+                  <td>Morning : </td>
+                  <td>Morning : </td>
+                  <td>Morning : </td>
+                </tr>
+                <tr className='p-5'>
+                  <td>
+                    <select>
+                      <option>11:00am</option>
+                      <option>12:00pm</option>
+                    </select>
+                  </td>
+                  <td>
+                    <select>
+                      <option>11:00am</option>
+                      <option>12:00pm</option>
+                    </select>
+                  </td>
+                  <td>
+                    <select>
+                      <option>11:00am</option>
+                      <option>12:00pm</option>
+                    </select>
+                  </td>
+                  <td>
+                    <select>
+                      <option>11:00am</option>
+                      <option>12:00pm</option>
+                    </select>
+                  </td>
+                  <td>
+                    <select>
+                      <option>11:00am</option>
+                      <option>12:00pm</option>
+                    </select>
+                  </td>
+                  <td>
+                    <select>
+                      <option>11:00am</option>
+                      <option>12:00pm</option>
+                    </select>
+                  </td>
+                </tr>
+                <tr className='p-5'>
+                  <td>Evening : </td>
+                </tr>
+                <tr className='p-5'>
+                  <td>
+                    <select>
+                      <option>5:00pm</option>
+                      <option>6:00pm</option>
+                      <option>7:00pm</option>
+                    </select>
+                  </td>
+                  <td>
+                    <select>
+                      <option>5:00pm</option>
+                      <option>6:00pm</option>
+                      <option>7:00pm</option>
+                    </select>
+                  </td>
+                  <td>
+                    <select>
+                      <option>5:00pm</option>
+                      <option>6:00pm</option>
+                      <option>7:00pm</option>
+                    </select>
+                  </td>
+                  <td>
+                    <select>
+                      <option>5:00pm</option>
+                      <option>6:00pm</option>
+                      <option>7:00pm</option>
+                    </select>
+                  </td>
+                  <td>
+                    <select>
+                      <option>5:00pm</option>
+                      <option>6:00pm</option>
+                      <option>7:00pm</option>
+                    </select>
+                  </td>
+                  <td>
+                    <select>
+                      <option>5:00pm</option>
+                      <option>6:00pm</option>
+                      <option>7:00pm</option>
+                    </select>
+                  </td>
+                </tr>
+              </table>
+          </div>
+          </fieldset>
+      <div className="form-group row m-2">
+        <div className="col-sm-10">
+          <button type="submit" className="btn btn-primary">Book Now</button>
         </div>
       </div>
-    </div>
-  </fieldset>
-  <div class="form-group row">
-    <div class="col-sm-2">Checkbox</div>
-    <div class="col-sm-10">
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" id="gridCheck1" />
-        <label class="form-check-label" for="gridCheck1">
-          Example checkbox
-        </label>
+        </form>
       </div>
-    </div>
-  </div>
-  <div class="form-group row">
-    <div class="col-sm-10">
-      <button type="submit" class="btn btn-primary">Sign in</button>
-    </div>
-  </div>
-</form>
     </>
   )
 }
